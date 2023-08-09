b98a3d29f5919cf98f3092cb7bdc53f5388f86d4
faiz033033 <feju302@gmail.com>
2023-08-09 21:40:03 +0530

    InitializeWithBasic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,66 @@
+import Header from './Components/Layout/Header'
+import PageSummary from './Components/UI/PageSummary'
+import Store from './Components/Store/Store'
+import Footer from './Components/Layout/Footer'
+
 function App() {
+
+  const productsArr = [
+
+    {
+
+    title: 'Colors',
+
+    price: 100,
+
+    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
+
+    key:Math.random().toString()
+
+    },
+
+    {
+
+    title: 'Black and white Colors',
+
+    price: 50,
+
+    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
+    key:Math.random().toString()
+
+    },
+
+    {
+
+    title: 'Yellow and Black Colors',
+
+    price: 70,
+
+    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
+    key:Math.random().toString()
+
+    },
+
+    {
+
+    title: 'Blue Color',
+
+    price: 100,
+
+    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%204.png',
+    key:Math.random().toString()
+
+    }
+
+    ]
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <>
+      <Header></Header>
+      <PageSummary></PageSummary>
+      <Store storeItems = {productsArr}></Store>
+      <Footer></Footer>
+    </>
+
   );
 }
 
---
+++ src/Components/Layout/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Facebooklogo from '../../assests/Facebooklogo.png'; 
+import Youtubelogo from '../../assests/YouTubelogo.jpg'; 
+import Spotifylogo from '../../assests/Spotifylogo.jpg'; 
+import './Footer.css';
+
+const Footer = (props) => {
+    return (
+        <div className="mainFooter">
+            <span className="footerImage">The Generics</span>
+            <div className="images">
+                <a href="https://www.youtube.com/" target="_blank"><img src={Youtubelogo} alt="Youtube Logo" className="youtube" /></a>
+                <a href="https://www.facebook.com/" target="_blank"><img src={Spotifylogo} alt="Spotify logo" className="spotify" /></a>
+                <a><img src={Facebooklogo} alt="Facebook Logo" className="facebook" /></a>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
