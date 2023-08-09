9ebc9078b67988ad84567d51f3c70a8079f83c1a
faiz033033 <feju302@gmail.com>
2023-08-09 22:02:14 +0530

    CartAdded

--- src/App.js
+++ src/App.js
@@ -1,66 +1,70 @@
-import Header from './Components/Layout/Header'
-import PageSummary from './Components/UI/PageSummary'
-import Store from './Components/Store/Store'
-import Footer from './Components/Layout/Footer'
+import { useState } from "react";
+import Header from "./Components/Layout/Header";
+import PageSummary from "./Components/UI/PageSummary";
+import Store from "./Components/Store/Store";
+import Footer from "./Components/Layout/Footer";
+import Cart from './Components/Cart/Cart'
 
 function App() {
+  const [cartClicked, setCartClicked] = useState(false);
+  const cartDisplayHandler = () => {
+    setCartClicked(true);
+  };
+
+  const cartDisplayHider = () => {
+    setCartClicked(false);
+  };
 
   const productsArr = [
-
     {
+      title: "Colors",
 
-    title: 'Colors',
+      price: 100,
 
-    price: 100,
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
 
-    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
-
-    key:Math.random().toString()
-
+      key: Math.random().toString(),
     },
 
     {
+      title: "Black and white Colors",
 
-    title: 'Black and white Colors',
+      price: 50,
 
-    price: 50,
-
-    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
-    key:Math.random().toString()
-
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
+      key: Math.random().toString(),
     },
 
     {
+      title: "Yellow and Black Colors",
 
-    title: 'Yellow and Black Colors',
+      price: 70,
 
-    price: 70,
-
-    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
-    key:Math.random().toString()
-
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
+      key: Math.random().toString(),
     },
 
     {
+      title: "Blue Color",
 
-    title: 'Blue Color',
+      price: 100,
 
-    price: 100,
-
-    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%204.png',
-    key:Math.random().toString()
-
-    }
-
-    ]
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
+      key: Math.random().toString(),
+    },
+  ];
   return (
     <>
-      <Header></Header>
+      <Header onClose={cartDisplayHandler}></Header>
       <PageSummary></PageSummary>
-      <Store storeItems = {productsArr}></Store>
+      <Store storeItems={productsArr}></Store>
       <Footer></Footer>
+      {cartClicked && <Cart onClose={cartDisplayHider}></Cart>}
     </>
-
   );
 }
 
--- src/Components/Layout/Footer.js
+++ src/Components/Layout/Footer.js
@@ -1,20 +1,27 @@
 import React from "react";
-import Facebooklogo from '../../assests/Facebooklogo.png'; 
-import Youtubelogo from '../../assests/YouTubelogo.jpg'; 
-import Spotifylogo from '../../assests/Spotifylogo.jpg'; 
-import './Footer.css';
+import Facebooklogo from "../../assests/Facebooklogo.png";
+import Youtubelogo from "../../assests/YouTubelogo.jpg";
+import Spotifylogo from "../../assests/Spotifylogo.jpg";
+import "./Footer.css";
 
 const Footer = (props) => {
-    return (
-        <div className="mainFooter">
-            <span className="footerImage">The Generics</span>
-            <div className="images">
-                <a href="https://www.youtube.com/" target="_blank"><img src={Youtubelogo} alt="Youtube Logo" className="youtube" /></a>
-                <a href="https://www.facebook.com/" target="_blank"><img src={Spotifylogo} alt="Spotify logo" className="spotify" /></a>
-                <a><img src={Facebooklogo} alt="Facebook Logo" className="facebook" /></a>
-            </div>
-        </div>
-    );
+  return (
+    <div className="mainFooter">
+      <span className="footerImage">The Generics</span>
+      <div className="images">
+        <a href="https://www.youtube.com/" target="blank">
+          <img src={Youtubelogo} alt="Youtube Logo" className="youtube" />
+        </a>
+        <a href="https://www.facebook.com/" target="blank">
+          <img src={Spotifylogo} alt="Spotify logo" className="spotify" />
+        </a>
+
+        <a href="www.facebook.com">
+          <img src={Facebooklogo} alt="Facebook Logo" className="facebook" />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
